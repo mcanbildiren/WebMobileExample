92480f769bfed1b17c052fd8315cc514b2258dbf
mcanbildiren <78155451+mcanbildiren@users.noreply.github.com>
2022-04-12 10:51:56 +0300

    Create Role List

--- Identity101/Controllers/AccountController.cs
+++ Identity101/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using Identity101.Models.Identity;
+using Identity101.Models.Role;
+using Identity101.Services.Email;
 using Identity101.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +10,28 @@ namespace Identity101.Controllers;
 public class AccountController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IEmailService _emailService;
+    private readonly RoleManager<ApplicationRole> _roleManager;
 
-    public AccountController(UserManager<ApplicationUser> userManager)
+    public AccountController(UserManager<ApplicationUser> userManager, IEmailService emailService, RoleManager<ApplicationRole> roleManager)
     {
         _userManager = userManager;
+        _emailService = _emailService;
+        _roleManager = roleManager;
+        CheckRoles();
+    }
+
+    private void CheckRoles()
+    {
+        foreach (var item in Roles.RoleList)
+        {
+            if (_roleManager.RoleExistsAsync(item).Result)
+                continue;
+            var result = _roleManager.CreateAsync(new ApplicationRole()
+            {
+                Name = item
+            }).Result;
+        }
     }
 
     [HttpGet("~/kayit-ol")]

63051cf1493aaa7e18bcbb1d1254c43703715b9e
mcanbildiren <78155451+mcanbildiren@users.noreply.github.com>
2022-04-14 14:47:31 +0300

    MVC

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<WebApplication1Context>(options =>
+    options.UseSqlite(connectionString));builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 

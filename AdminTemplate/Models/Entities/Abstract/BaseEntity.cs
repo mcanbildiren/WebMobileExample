fbad522f17af631c18ff41f64a494b49227c3f8e
mcanbildiren <78155451+mcanbildiren@users.noreply.github.com>
2022-04-19 15:02:01 +0300

    abstract category, abstract product, create database

---
+++ AdminTemplate/Models/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminTemplate.Models.Entities.Abstract
+{
+    public abstract class BaseEntity<T> where T : IEquatable<T> //equatable: karşılaştırılabilir nesneler
+    {
+        public T Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string CreatedUser { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? UpdatedUser { get; set; }
+    }
+}
---
+++ AdminTemplate/Models/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using AdminTemplate.Models.Entities;
+using AdminTemplate.Models.Entities.Abstract;
+
+public class Product : BaseEntity<Guid>
+{
+    public string Name { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}

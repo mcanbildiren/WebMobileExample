478645964f7d456ed1e192798df68ebffb21131d
mcanbildiren <78155451+mcanbildiren@users.noreply.github.com>
2022-04-22 17:24:15 +0300

    .

---
+++ AdminTemplate/ViewModels/ProfileUpdateViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminTemplate.ViewModels
+{
+    public class ProfileUpdateViewModel
+    {
+        public ResetPasswordViewModel ResetPassword { get; set; } 
+
+        public UserProfileViewModel UserProfile { get; set; }
+    }
+}

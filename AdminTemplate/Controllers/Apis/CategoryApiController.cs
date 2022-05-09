1c936c87ab72533c85837d403107d054b58a4561
mcanbildiren <78155451+mcanbildiren@users.noreply.github.com>
2022-05-09 14:32:43 +0300

    category -- add, update, delete

--- AdminTemplate/Controllers/Apis/CategoryApiController.cs
+++ AdminTemplate/Controllers/Apis/CategoryApiController.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AdminTemplate.Controllers.Apis
 {
-    [Route("api/[controller]/[action]")]
+    //[Route("api/[controller]/[action]/{id?}")]
     [ApiController]
-    public class CategoryApiController : ControllerBase
+    public class CategoryApiController : BaseApiController
     {
         private readonly MyContext _context;
 
@@ -42,11 +42,78 @@ namespace AdminTemplate.Controllers.Apis
             {
                 model.CreatedUser = HttpContext.User.Identity!.Name;
                 _context.Categories.Add(model);
-                 _context.SaveChanges();
+                _context.SaveChanges();
                 return Ok(new
                 {
                     Success = true,
                     Message = $"{model.Name} isimli kategori başarıyla eklendi"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = $"Bir hata oluştu: {ex.Message}" });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Detail(int id = 0)
+        {
+            try
+            {
+                return Ok(_context.Categories.Find(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = $"Bir hata oluştu: {ex.Message}" });
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update(int id, Category model)
+        {
+            try
+            {
+                var category = _context.Categories.Find(id);
+
+                if (category == null)
+                {
+                    return NotFound(new { Message = $"{id} numaralı kategori bulunamadı" });
+                }
+
+                category.Name = model.Name;
+                category.Description = model.Description;
+                category.UpdatedUser = HttpContext.User.Identity!.Name;
+                category.UpdatedDate = DateTime.UtcNow;
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    Success = true,
+                    Message = $"{category.Name} isimli kategori başarıyla güncellendi"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = $"Bir hata oluştu: {ex.Message}" });
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var category = _context.Categories.Find(id);
+
+                if (category == null)
+                {
+                    return NotFound(new { Message = $"{id} numaralı kategori bulunamadı" });
+                }
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    Success = true,
+                    Message = $"{category.Name} isimli kategori başarıyla silindi"
                 });
             }
             catch (Exception ex)
